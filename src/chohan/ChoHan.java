4fc1d30f5526a5098eb9d939b08f9de12c99d286
Mikey Neff <mjneff922@gmail.com>
2018-12-16 13:34:38 -0600

    Initial Commit

---
+++ src/chohan/ChoHan.java
@@ -0,0 +1,77 @@
+package chohan;
+
+import java.util.Scanner;
+
+public class ChoHan {
+    public static void main(String[] args) {
+        Die die1 = new Die();
+        Die die2 = new Die();
+        Scanner sc = new Scanner(System.in);
+        Scanner scNum = new Scanner(System.in);
+        
+        System.out.print("How much money would you like to start with? ");
+        double money = scNum.nextDouble();
+        
+        while (money > 0) {
+            System.out.print("Will the dice sum to odd or even? ");
+            String pick = sc.nextLine();
+            int pickValue = parse(pick);
+            System.out.print("How much do you wish to bet on this? ");
+            double bet = scNum.nextDouble();
+        
+            die1.roll();
+            die2.roll();
+            System.out.println("Die 1: " + die1.getNumDots() + " Die 2: " + die2.getNumDots());
+            int total = die1.getNumDots() + die2.getNumDots();
+        
+            int result = processRoll(total);
+            if (pickValue == result) {
+                System.out.printf("You won $%3.2f!%n", (bet * 2));
+                money += bet;
+            }
+            else {
+                System.out.printf("You lost your bet of $%3.2f.%n", bet);
+                money -= bet;
+            }
+            System.out.printf("You have $%3.2f remaining. Would you like to continue playing (Y/N)? ", money);
+            if (sc.nextLine().equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+        System.out.printf("%nYou finished with $%3.2f. Thanks for playing!", money);
+    }
+    
+    public static int parse(String s) {
+        if (s.equalsIgnoreCase("Even")) {
+            return 1;
+        }
+        else if (s.equalsIgnoreCase("Odd")) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    public static int processRoll(int total) {
+        switch(total) {
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+            case 12:
+                return 1;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+            case 11:
+                return -1;
+            default:
+                return 0;
+        }
+    }
+    
+}
---
+++ src/chohan/Die.java
@@ -0,0 +1,20 @@
+package chohan;
+
+/**
+ *
+ * @author Mikey
+ */
+public class Die {
+    private int numDots;
+    
+    public Die() {
+    }
+    
+    public void roll() {
+        numDots = (int)(1 + 6 * Math.random());
+    }
+    
+    public int getNumDots() {
+        return numDots;
+    }
+}
